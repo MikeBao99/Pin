08e5d4866ecb2633ac61885822dd36b7a3c6ffb5
Mike Bao <mikebao@college.harvard.edu>
2017-11-18 16:20:44 -0500

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,11 @@ from flask import *
 
 app = Flask(__name__)
 
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 @app.route('/', methods=["GET", "POST"])
 def homepage():  
 	return render_template('homepage.html')
