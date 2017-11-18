2ae05627e2fa4f8d3497b38dde90b6f78f51f056
Mike Bao <mikebao@college.harvard.edu>
2017-11-18 15:20:09 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -10,32 +10,32 @@ views = Blueprint('views', __name__)
 
 extensions = set(['jpg'])
 
-@views.route('/', methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
 def homepage():
 	return render_template('homepage.html')
 
-@views.route('/register', methods=["GET", "POST"])
+@app.route('/register', methods=["GET", "POST"])
 def register():
 	if request.method == "POST":
 		return redirect("/")
 	else:
 		return render_template('register.html')
 
-@views.route('/login', methods=["GET", "POST"])
+@app.route('/login', methods=["GET", "POST"])
 def login():
 	if request.method == "POST":
 		return redirect("/")
 	else:
 		return render_template('login.html')
 	
-@views.route('/create', methods=["GET", "POST"])
+@app.route('/create', methods=["GET", "POST"])
 def create():
 	if request.method == "POST":
 		return redirect("/")
 	else:
 		return render_template('create.html')
 	
-@views.route('/about')
+@app.route('/about')
 def about():
 	return render_template('about.html')
 
