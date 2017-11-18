0c30b18816f0ecd0f1eb08fb09b67c06a8bae5f6
Mike Bao <mikebao@college.harvard.edu>
2017-11-18 01:56:19 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -11,7 +11,14 @@ extensions = set(['jpg'])
 @views.route('/', methods=["GET", "POST"])
 def homepage():
 	return render_template('homepage.html')
-  
+
+@views.route('/register', methods=["GET" "POST"])
+def register():
+	if request.method == "POST":
+		redirect("/")
+	else:
+		render_template('register.html')
+	
 @views.route('/about')
 def about():
 	return render_template('about.html')
