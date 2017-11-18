d1b46c009086b8b9034b654268a58bcc99a1a691
Mike Bao <mikebao@college.harvard.edu>
2017-11-18 16:31:27 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+from sqlalchemy import create_engine
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
@@ -12,6 +13,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+db = create_engine('postgres://lgexotyvoyetfh:0c61846252d51c314b5f925d9729ad5bc818ca97deb54cc9592759bfe4e2e2ab@ec2-107-20-214-99.compute-1.amazonaws.com:5432/d223jo1u4l3s6f')
+			
 @app.route('/', methods=["GET", "POST"])
 def homepage():  
 	return render_template('homepage.html')
