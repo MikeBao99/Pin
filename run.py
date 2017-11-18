d497191bbe7a12d280692842b84f58f2e39da970
Mike Bao <mikebao@college.harvard.edu>
2017-11-18 16:35:35 -0500

    Update run.py

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ Session(app)
 
 # Configure PostgreSQL database using SQLAlchemy
 db = create_engine('postgres://lgexotyvoyetfh:0c61846252d51c314b5f925d9729ad5bc818ca97deb54cc9592759bfe4e2e2ab@ec2-107-20-214-99.compute-1.amazonaws.com:5432/d223jo1u4l3s6f')
-db = create_engine(db_string)
+
 
 @app.route('/', methods=["GET", "POST"])
 def homepage():  
