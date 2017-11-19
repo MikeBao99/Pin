18b9bd0db74e4391d181f58aec854f94eceee014
Mike Bao <mikebao@college.harvard.edu>
2017-11-19 11:43:23 -0500

    Update run.py

--- run.py
+++ run.py
@@ -78,8 +78,9 @@ def login():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
-	
+
 @app.route('/create', methods=["GET", "POST"])
+@login_required	
 def create():
 	if request.method == "POST":
 		return redirect("/")
@@ -92,6 +93,17 @@ def manage():
 		return redirect("/")
 	else:
 		return render_template('manage.html')
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
 
 # go to localhost:8000 to view
 if 'DEBUG' in os.environ:
