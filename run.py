ee29e4c59961fb9bfdd07030943be0145ac796f5
Mike Bao <mikebao@college.harvard.edu>
2017-11-19 11:27:03 -0500

    Implement login

--- run.py
+++ run.py
@@ -48,10 +48,36 @@ def register():
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
-	if request.method == "POST":
-		return redirect("/")
-	else:
-		return render_template('login.html')
+	# Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("error.html")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("error.html")
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = '%s'" % (request.form.get("username")))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows.fetchone()["hash"], request.form.get("password")):
+            return render_template("error.html")
+
+        # Remember which user has logged in
+        session["user_id"] = rows.fetchone()["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
 	
 @app.route('/create', methods=["GET", "POST"])
 def create():
