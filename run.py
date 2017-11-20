4ca50eddde22e2fc9f9e37b6579471f45a585267
Mike Bao <mikebao@college.harvard.edu>
2017-11-20 15:40:25 -0500

    Update run.py

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 app = Flask(__name__)
 
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = True
+app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
@@ -52,7 +52,7 @@ def register():
 @app.route('/login', methods=["GET", "POST"])
 def login():
 	# Forget any user_id
-    session.clear()
+    
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
