36f817a57ed0caa364734a1e601b932ccb6c50f3
Mike Bao <mikebao@college.harvard.edu>
2017-11-20 15:59:36 -0500

    Update run.py

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+#app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure PostgreSQL database using SQLAlchemy
@@ -26,15 +26,15 @@ def register():
 	if request.method == "POST":
 		# Ensure username was submitted
 		if not request.form.get("username"):
-			return redirect("/error", error="Please provide username!")
+			return redirect("/error")
 		# Ensure password was submitted
-		if not request.form.get("password", error="Please provide password!"):
+		if not request.form.get("password"):
 			return redirect("/error")
 		if not request.form.get("confirmation"):
-			return redirect("/error", error="Please confirm password!")
+			return redirect("/error")
 
 		if request.form.get("password") != request.form.get("confirmation"):
-			return redirect("/error", error="Passwords do not match!")
+			return redirect("/error")
 		password_hash = generate_password_hash(request.form.get("password"))
 		try:
 			db.execute("INSERT INTO users (username, hash) VALUES('%s', '%s')" % (request.form.get("username"), password_hash))
@@ -50,18 +50,18 @@ def register():
 @app.route('/login', methods=["GET", "POST"])
 def login():
 	# Forget any user_id
-    
+    session.clear()
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return redirect("/error", error="Please provide username!")
+            return redirect("/error")
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return redirect("/error", error="Please provide password!")
+            return redirect("/error")
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = '%s'" % (request.form.get("username")))
