287230a71b76a30795ad9c94e7349469d728ebec
Mike Bao <mikebao@college.harvard.edu>
2017-11-28 22:10:10 -0500

    Update run.py

--- run.py
+++ run.py
@@ -10,9 +10,9 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
-app.config["SESSION_FILE_DIR"] = mkdtemp()
+# app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_TYPE"] = "redis"
 Session(app)
 
 if app.config["DEBUG"]:
