ebef441b6ae3fb418720fcbd32e55eb5f629f62f
Mike Bao <mikebao@college.harvard.edu>
2017-11-29 20:00:48 -0500

    Update run.py

--- run.py
+++ run.py
@@ -19,6 +19,7 @@ if app.config["DEBUG"]:
     @app.after_request
     def after_request(response):
         response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+	response.cache_control.max_age = 0
         response.headers["Expires"] = 0
         response.headers["Pragma"] = "no-cache"
         return response
