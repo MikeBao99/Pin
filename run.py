7f1ba305b09bc5a080b099d6c6b78db354a3ea46
Mike Bao <mikebao@college.harvard.edu>
2017-11-29 20:09:27 -0500

    Update run.py

--- run.py
+++ run.py
@@ -8,20 +8,24 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
 
+
+
 app = Flask(__name__)
+
+#if app.config["DEBUG"]:
+@app.after_request
+def after_request(response):
+	response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+	response.headers["Expires"] = 0
+	response.headers["Pragma"] = "no-cache"
+	return response
 
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-if app.config["DEBUG"]:
-    @app.after_request
-    def after_request(response):
-        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-        response.headers["Expires"] = 0
-        response.headers["Pragma"] = "no-cache"
-        return response
+
 
 
 # Configure PostgreSQL database using SQLAlchemy
