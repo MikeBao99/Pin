7c69d73e68379c0f3e756b0539f7a0f9735484da
Mike Bao <mikebao@college.harvard.edu>
2017-12-01 01:35:02 -0500

    Update run.py

--- run.py
+++ run.py
@@ -191,9 +191,8 @@ def edit():
         val = request.form.get("edit")
         eventsrow = db.execute("SELECT * FROM events WHERE id = '%s'" % (val))
         events= eventsrow.fetchone()
-        print events["starttime"]
-        sys.stdout.flush()
-        #events["endtime"] = events["endtime"].replace(" ","T")
+        events["starttime"] = replace_spaces(events["starttime"])
+        events["endtime"] = replace_spaces(events["endtime"])
         return render_template('edit.html', events = events)
     
 @app.route('/delete', methods=["GET", "POST"])
