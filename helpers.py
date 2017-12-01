41df2a153f8475f35600de6c93454001bef0b4f8
Mike Bao <mikebao@college.harvard.edu>
2017-12-01 01:37:17 -0500

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -2,15 +2,6 @@ from functools import wraps
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 
-def replace_spaces(word):
-    answer = ""
-    for letter in word:
-        if letter == " ":
-            answer = answer + "T"
-        else:
-            answer = answer + letter
-    return answer
-
 def login_required(f):
     """
     Decorate routes to require login.
