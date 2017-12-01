7925d50d3f75b1c2f9041dcacf62e0e193639e01
Mike Bao <mikebao@college.harvard.edu>
2017-11-30 21:51:22 -0500

    Update run.py

--- run.py
+++ run.py
@@ -144,7 +144,7 @@ def create():
 @app.route('/search', methods=["GET", "POST"])
 def search():
 	if request.method == "POST":
-		q = request.form.get("search") + "%"
+		q = request.form.get("search") + "%%"
     		eventsrow = db.execute("SELECT * FROM events WHERE class LIKE '%s'" % (q))
 		events = []
 		row = eventsrow.fetchone()
