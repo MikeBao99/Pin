a01caada500d1842b3a708533c21a559485e452c
jjhuang22 <31576383+jjhuang22@users.noreply.github.com>
2017-11-30 23:17:02 -0500

    Update run.py

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
-from time import localtime, strftime
+from datetime import datetime
 
 
 app = Flask(__name__)
