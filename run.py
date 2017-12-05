0be2e9409b18be86d1ed9eed836ea1c5052d86d9
Mike Bao <mikebao@college.harvard.edu>
2017-12-05 14:38:26 -0500

    Update run.py

--- run.py
+++ run.py
@@ -9,19 +9,20 @@ from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
-
+# Initialize Flask app
 app = Flask(__name__)
 app.jinja_env.auto_reload = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-#if app.config["DEBUG"]:
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
 
+# Set up Flask Session
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -33,10 +34,12 @@ Session(app)
 # Configure PostgreSQL database using SQLAlchemy
 db = create_engine('postgres://lgexotyvoyetfh:0c61846252d51c314b5f925d9729ad5bc818ca97deb54cc9592759bfe4e2e2ab@ec2-107-20-214-99.compute-1.amazonaws.com:5432/d223jo1u4l3s6f')
 
+# Renders Homepage
 @app.route('/', methods=["GET", "POST"])
 def homepage():  
     return render_template('homepage.html')
 
+# Renders and handles submission of Register page
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -48,7 +51,7 @@ def register():
             return render_template("error.html", error = "Please provide password!")
         if not request.form.get("confirmation"):
             return render_template("error.html", error = "Please provide password confirmation!")
-
+        # Ensure that passwords match
         if request.form.get("password") != request.form.get("confirmation"):
             return render_template("error.html", error = "Passwords do not match!")
         password_hash = generate_password_hash(request.form.get("password"))
@@ -63,6 +66,7 @@ def register():
     else:
         return render_template("register.html")
 
+# Logs users in, renders login.html and handles form submission
 @app.route('/login', methods=["GET", "POST"])
 def login():
     # Forget any user_id
@@ -108,10 +112,12 @@ def logout():
     # Redirect user to login form
     return redirect("/")
 
+# Renders an error page
 @app.route('/error', methods=["GET"])
 def error():
        return render_template("error.html")
 
+# Renders the Create Event page and handles form submission
 @app.route('/create', methods=["GET", "POST"])
 @login_required 
 def create():
@@ -148,10 +154,14 @@ def create():
         return redirect("/")
     else:
         return render_template('create.html')
+    
+# Handles the search function, searches through the SQL database with several conditions
 @app.route('/search', methods=["GET", "POST"])
 def search():
     if request.method == "POST":
+        # Store user search request
         q = "%%" + request.form.get("search") + "%%"
+        # Query Database
         eventsrow = db.execute("SELECT * FROM events WHERE LOWER(class) LIKE LOWER('%s') AND endtime > CURRENT_TIMESTAMP ORDER BY starttime ASC" % (q))
         events = []
         row = eventsrow.fetchone()
@@ -160,7 +170,7 @@ def search():
             row = eventsrow.fetchone()
         return render_template('search.html', events = events)
     else:
-
+        # Prints out all events in a table, Query database
         eventsrow = db.execute("SELECT * FROM events AND endtime > CURRENT_TIMESTAMP ORDER BY starttime ASC")
         events = []
         row = eventsrow.fetchone()
@@ -169,7 +179,7 @@ def search():
             row = eventsrow.fetchone()
         return render_template('search.html', events = events)
 
-    
+# Handles the manage 
 @app.route('/manage', methods=["GET", "POST"])
 def manage():
     eventsrow = db.execute("SELECT * FROM events WHERE endtime > CURRENT_TIMESTAMP AND name = '%s'" % (session["user_id"]))
@@ -180,24 +190,27 @@ def manage():
         row = eventsrow.fetchone()
     return render_template('manage.html', events = events, name = session["user_id"])
 
+# Handles Edit if a user chooses to edit a page from the Manage Events page
 @app.route('/edit', methods=["GET", "POST"])
 def edit():
     if request.form.get("type") == "post":
         print request.form.get("startDatetime") + "\n\n"
         sys.stdout.flush()
+        # Update the database
         db.execute("UPDATE events SET class = '%s', location = '%s', starttime = '%s', endtime = '%s' WHERE id = '%s'" % (request.form.get('class'), request.form.get('location'), request.form.get('startDatetime'), request.form.get('endDatetime'), request.form.get('id')))
         return redirect("/manage")
     else:
+        # Render the form for editing
         val = request.form.get("edit")
         eventsrow = db.execute("SELECT * FROM events WHERE id = '%s'" % (val))
         events= eventsrow.fetchone()
-#         events["starttime"] = replace_spaces(str(events["starttime"]))
-#         events["endtime"] = replace_spaces(str(events["endtime"]))
         return render_template('edit.html', events = events, starttime = replace_spaces(str(events["starttime"])), endtime = replace_spaces(str(events["endtime"])))
-    
+
+# Handles deletion of an event    
 @app.route('/delete', methods=["GET", "POST"])
 def delete():
     val = request.form.get('delete')
+    # Delete event from database
     db.execute("DELETE FROM events WHERE id = '%s'" % (val))
     return redirect('/manage')
 
